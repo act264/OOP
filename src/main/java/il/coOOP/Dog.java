42c868a86f0b5b81f8ad153d28e08bda256f21a6
Alex Berger <alexber@matrix.co.il>
2020-01-07 17:29:41 +0200

    adding params

--- src/main/java/il/coOOP/Dog.java
+++ src/main/java/il/coOOP/Dog.java
@@ -2,10 +2,34 @@ package il.coOOP;
 
 public class Dog {
 
-	int pows = 4;
-	int tail = 1;
-	String name;
-	String breed;
+	public int getPaws() {
+		return paws;
+	}
+	public void setPaws(int paws) {
+		this.paws = paws;
+	}
+	public int getTail() {
+		return tail;
+	}
+	public void setTail(int tail) {
+		this.tail = tail;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBreed() {
+		return breed;
+	}
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+	public int paws = 4;
+	public int tail = 1;
+	public String name;
+	public String breed;
 	
 	
 }
